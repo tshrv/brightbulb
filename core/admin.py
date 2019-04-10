a0d603dbd5a5073dd3a97764cb22fcb73607ecfa
Tushar Srivastava <me@yourexample.com>
2019-04-11 01:45:44 +0530

    notes crud

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = 'treetrails.'
+admin.site.site_title = 'treetrails.'
+admin.site.index_title = 'Administration'
